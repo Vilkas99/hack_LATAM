9b62d9c6819e59261202462508e3f1de4f466295
anderasdfg <anderleyc@gmail.com>
2021-02-20 00:47:02 -0500

    Add imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import 'antd/dist/antd.css'; 
-
-=======
+import React from 'react';
 import logo from "./logo.svg";
 import "./App.css";
 import VideoLayout from "./Components/VideoLayout";
 import "antd/dist/antd.css";
->>>>>>> 83a1df5a18628da25ea4fc8df43cfb1968685795
 
 function App() {
   return <VideoLayout />;
